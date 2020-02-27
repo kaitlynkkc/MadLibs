29023f9c1dff2241f7f84aa812c5061a2c4706f4
kaitlynkkc <campbellkk123@gmail.com>
2020-02-27 01:08:29 -0600

    First commit, Initial Layout Only

---
+++ app/src/main/java/com/example/madlibs/ReadStory.kt
@@ -0,0 +1,12 @@
+package com.example.madlibs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadStory : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_story)
+    }
+}
